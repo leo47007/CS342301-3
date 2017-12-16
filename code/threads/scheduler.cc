636f19423433948b02cd1f56e84f565a0d94ccb7
蕭育舜 <leo47007@gmail.com>
2017-12-17 01:26:53 +0800

    debug

--- code/threads/scheduler.cc
+++ code/threads/scheduler.cc
@@ -102,11 +102,7 @@ Scheduler::ReadyToRun (Thread *thread)
         if(thread->getBurstTime() < kernel->currentThread->getBurstTime())
         {
             kernel->currentThread->Yield();
-<<<<<<< HEAD
             kernel->currentThread->setTmpburstTime(kernel->currentThread->getTmpburstTime()+(kernel->stats->totalTicks - kernel->currentThread->getStartExeTime()));
-=======
-            kernel->currentThread->setTmpburstTime(kernel->currentThread->getTmpburstTime() +(kernel->stats->totalTicks - kernel->currentThread->getStartExeTime()));
->>>>>>> 240111b95684130bd7bf6e07436dd3ef410cca29
         }
     }
     else if(thread->getPriority() >= 50)
