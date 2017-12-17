98141b65d86b676e2a7987557d217f24b25595c5
liyeouwang <32289058+liyeouwang@users.noreply.github.com>
2017-12-17 14:27:46 +0800

    debug

--- code/machine/interrupt.cc
+++ code/machine/interrupt.cc
@@ -173,7 +173,7 @@ Interrupt::OneTick()
    // if (yieldOnReturn) {	// if the timer device handler asked  //leo comment
     				// for a context switch, ok to do it now  
     cout<<"kernel->stats->totalTicks = "<<kernel->stats->totalTicks<<endl;
-    cout<<"(onetick)kernel->currentThread->getStartExeTime="<<kernel->currentThread->getStartExeTime<<endl;
+    cout<<"(onetick)kernel->currentThread->getStartExeTime()="<<kernel->currentThread->getStartExeTime()<<endl;
     if (yieldOnReturn && kernel->currentThread->getPriority()<50 && (kernel->stats->totalTicks-kernel->currentThread->getStartExeTime()%100 == 0)) { //leo add 
         cout<<"in yieldOnReturn"<<endl;
 	yieldOnReturn = FALSE;
